8093502f3e02afd7553ad80dbb48523f6163960d
Bhavdeep Singh Nijhawan <143419096+BhavdeepSinghNijhawan@users.noreply.github.com>
2024-06-06 16:46:17 +0530

    Update SeeNotice.js

--- frontend/src/components/SeeNotice.js
+++ frontend/src/components/SeeNotice.js
@@ -1,30 +1,34 @@
-//Author: Bhavdeep Singh Nijhawan
+// Author: Bhavdeep Singh Nijhawan
 
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllNotices } from '../redux/noticeRelated/noticeHandle';
 import { Paper } from '@mui/material';
 import TableViewTemplate from './TableViewTemplate';
+
 const SeeNotice = () => {
     const dispatch = useDispatch();
     const { currentUser, currentRole } = useSelector(state => state.user);
     const { noticesList, loading, error, response } = useSelector((state) => state.notice);
+
     useEffect(() => {
         if (currentRole === "Admin") {
             dispatch(getAllNotices(currentUser._id, "Notice"));
-        }
-        else {
+        } else {
             dispatch(getAllNotices(currentUser.school._id, "Notice"));
         }
-    }, [dispatch]);
+    }, [dispatch, currentUser, currentRole]);
+
     if (error) {
         console.log(error);
     }
+
     const noticeColumns = [
         { id: 'title', label: 'Title', minWidth: 170 },
         { id: 'details', label: 'Details', minWidth: 100 },
         { id: 'date', label: 'Date', minWidth: 170 },
     ];
+
     const noticeRows = noticesList.map((notice) => {
         const date = new Date(notice.date);
         const dateString = date.toString() !== "Invalid Date" ? date.toISOString().substring(0, 10) : "Invalid Date";
@@ -35,6 +39,7 @@ const SeeNotice = () => {
             id: notice._id,
         };
     });
+
     return (
         <div style={{ marginTop: '50px', marginRight: '20px' }}>
             {loading ? (
@@ -45,13 +50,14 @@ const SeeNotice = () => {
                 <>
                     <h3 style={{ fontSize: '30px', marginBottom: '40px' }}>Notices</h3>
                     <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-                        {Array.isArray(noticesList) && noticesList.length > 0 &&
+                        {Array.isArray(noticesList) && noticesList.length > 0 && (
                             <TableViewTemplate columns={noticeColumns} rows={noticeRows} />
-                        }
+                        )}
                     </Paper>
                 </>
             )}
         </div>
-    )
-}
-export default SeeNotice
+    );
+};
+
+export default SeeNotice;
