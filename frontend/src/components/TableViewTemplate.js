28d5a7fd37ddfd969a805b057553a03d2bc4ad33
Bhavdeep Singh Nijhawan <143419096+BhavdeepSinghNijhawan@users.noreply.github.com>
2024-06-06 16:49:20 +0530

    Update TableViewTemplate.js

--- frontend/src/components/TableViewTemplate.js
+++ frontend/src/components/TableViewTemplate.js
@@ -1,11 +1,13 @@
-//Author: Bhavdeep Singh Nijhawan
+// Author: Bhavdeep Singh Nijhawan
 
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { StyledTableCell, StyledTableRow } from './styles';
 import { Table, TableBody, TableContainer, TableHead, TablePagination } from '@mui/material';
+
 const TableViewTemplate = ({ columns, rows }) => {
     const [page, setPage] = useState(0);
     const [rowsPerPage, setRowsPerPage] = useState(5);
+
     return (
         <>
             <TableContainer>
@@ -26,24 +28,20 @@ const TableViewTemplate = ({ columns, rows }) => {
                     <TableBody>
                         {rows
                             .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                            .map((row) => {
-                                return (
-                                    <StyledTableRow hover role="checkbox" tabIndex={-1} key={row.id}>
-                                        {columns.map((column, index) => {
-                                            const value = row[column.id];
-                                            return (
-                                                <StyledTableCell key={index} align={column.align}>
-                                                    {
-                                                        column.format && typeof value === 'number'
-                                                            ? column.format(value)
-                                                            : value
-                                                    }
-                                                </StyledTableCell>
-                                            );
-                                        })}
-                                    </StyledTableRow>
-                                );
-                            })}
+                            .map((row) => (
+                                <StyledTableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                                    {columns.map((column, index) => {
+                                        const value = row[column.id];
+                                        return (
+                                            <StyledTableCell key={index} align={column.align}>
+                                                {column.format && typeof value === 'number'
+                                                    ? column.format(value)
+                                                    : value}
+                                            </StyledTableCell>
+                                        );
+                                    })}
+                                </StyledTableRow>
+                            ))}
                     </TableBody>
                 </Table>
             </TableContainer>
@@ -55,12 +53,12 @@ const TableViewTemplate = ({ columns, rows }) => {
                 page={page}
                 onPageChange={(event, newPage) => setPage(newPage)}
                 onRowsPerPageChange={(event) => {
-                    setRowsPerPage(parseInt(event.target.value, 5));
+                    setRowsPerPage(parseInt(event.target.value, 10));
                     setPage(0);
                 }}
             />
         </>
-    )
-}
+    );
+};
 
-export default TableViewTemplate
+export default TableViewTemplate;
