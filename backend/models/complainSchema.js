f010aa817eb3a66e8d03e72159fb07f351f5474d
Bhavdeep Singh Nijhawan <143419096+BhavdeepSinghNijhawan@users.noreply.github.com>
2024-07-10 10:10:14 +0530

    Update complainSchema.js

--- backend/models/complainSchema.js
+++ backend/models/complainSchema.js
@@ -1,24 +1,28 @@
+// Author: Bhavdeep Singh Nijhawan
+
 const mongoose = require('mongoose');
 
+// Define the schema for the 'complain' collection
 const complainSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'student',
-        required: true
+        ref: 'student', // Reference to the 'student' collection
+        required: true // User is a required field
     },
     date: {
         type: Date,
-        required: true
+        required: true // Date is a required field
     },
     complaint: {
         type: String,
-        required: true
+        required: true // Complaint text is a required field
     },
     school: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'admin',
-        required: true,
+        ref: 'admin', // Reference to the 'admin' collection
+        required: true // School is a required field
     }
 });
 
+// Export the model based on the schema
 module.exports = mongoose.model("complain", complainSchema);
